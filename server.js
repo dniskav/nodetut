0e10bc7d32d6161b213683019db6ae7451e137c7
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-11-21 13:09:06 -0500

    end of the part one os the tut

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ var url = require("url");
 
 function iniciar(route, handle) {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Peticion para " + pathname + " recibida.");
+        var dataPosteada = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Peticion para " + pathname + " recibida.");
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    var content = route(handle, pathname)
-    response.write(content);
-    response.end();
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(trozoPosteado) {
+          dataPosteada += trozoPosteado;
+          console.log("Recibido trozo POST '" + trozoPosteado + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, dataPosteada);
+    });
+
   }
 
   http.createServer(onRequest).listen(8080);
-  console.log("Servidor Iniciado.");
+  console.log("Servidor Iniciado");
 }
 
 exports.iniciar = iniciar;
