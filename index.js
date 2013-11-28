6456a8a73fc6d95d4838789af235f318f2ba2865
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-11-28 13:33:42 -0500

    basic server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var server = require('./server'),
 	route = require('./route'),
 	paths = require('./paths'),
+	path = require('path'),//helps with file paths
+	fs = require('fs'),//helps with file system tasks
 	requesthandlers = require('./requesthandlers'),
 	handle = paths.get(requesthandlers);
 
