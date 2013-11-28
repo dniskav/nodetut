c145cbc33309600e9b763fc197eb74a3a3f4a26e
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-11-28 09:55:43 -0500

    sorting the code

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,14 @@
-function home(){
-	return "<h1>I'm a call to home page</h1>";
+function home(response){
+	var content =  "<h1>I'm a call to home page</h1>";
+	response.writeHead(200, {'Content-Type' : 'text/html'});
+	response.write(content);
+	response.end();
 }
-function subir(){
-	return "<h1>I'm a call to upload page</h1>";
+function subir(response){
+	var content =  "<h1>I'm a call to upload page</h1>";
+	response.writeHead(200, {'Content-Type' : 'text/html'});
+	response.write(content);
+	response.end();
 }
 exports.home = home,
 exports.subir = subir;
--- route.js
+++ route.js
@@ -1,14 +1,14 @@
 function route(pathname, handle, response){
 
 	if(typeof handle[pathname] === 'function'){
-		response.writeHead(200, {'Content-Type' : 'text/html'});
-		var content = handle[pathname]();
+		handle[pathname](response);
 	}else{
 		response.writeHead(404, {'Content-Type' : 'text/html'});
 		var content = 'error 404 no encontrado';
+		response.write(content);
+		response.end();
 	};
-	response.write(content);
-	response.end();
+
 }
 
 exports.route = route;
